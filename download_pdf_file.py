73892ec51c7dd347ae7fce00a8954331e9622d36
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2021-11-22 20:23:06 +0200

    Update download_pdf_file.py

--- download_pdf_file.py
+++ download_pdf_file.py
@@ -5,3 +5,14 @@ r = requests.get(url)
 
 with open('filename.pdf', 'wb') as fd:
     fd.write(r.content)
+
+    
+# Иногда файл может быть в закодирован в формате base64 тогда чтобы его записать делаем так
+import base64
+dataStr = response.json()['Data']
+base64EncodedStr = base64.b64decode(dataStr.encode('utf-8'))
+
+# create pdf file
+with open('filename.pdf', 'wb') as f:
+    f.write(base64EncodedStr)
+    

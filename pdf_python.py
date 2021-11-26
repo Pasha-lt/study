e3e29a51dc3f3edf43a167916867624362821803
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2021-11-26 19:10:52 +0200

    Update pdf_python.py

--- pdf_python.py
+++ pdf_python.py
@@ -1,3 +1,5 @@
+import pdfplumber
+
 # Вытягиваем текствое содержимое из PDF файла.
 with pdfplumber.open('filename.pdf') as f: # открываем файл что бы не перегружать оперативку большим файлом выводим постранично и сравниваем
     entrance_msa = any(['Нужная нам строчка' in i.extract_text() for i in f.pages]) # Используя any если будет хобя бы 1 совпадение вернет True

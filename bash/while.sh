aaf20d8495b2953bc08e8553e45918465f969c64
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2021-12-09 22:01:38 +0200

    Update while.sh

--- bash/while.sh
+++ bash/while.sh
@@ -5,3 +5,11 @@ do
     echo "The counter is $COUNTER"
     let COUNTER=COUNTER+1
 done
+
+# Обработка входящих параметров вызов bash script.sh 23 35 'два'
+#!/usr/bin/env bash
+while [ "$1" != "" ]
+do
+    echo "Parameter equals $1"
+    shift
+done

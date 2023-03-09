97e4a1498db0e057c0fdfd8ed2f8d3da98c27a67
Pasha-lt <9139136@gmail.com>
2023-03-09 15:07:32 -0500

    simple task OOP

---
+++ OOP/OOP_tasks/basic_OOP/declare_simple_class_with_parameters.py
@@ -0,0 +1,17 @@
+# Declare a class named DataBase in Python that would store the following information:
+#
+# pk: 1
+# title: "Classes and Objects"
+# author: "Pavlo Kostyshen"
+# views: 14356
+# comments: 12
+#
+# Use the variable names (class attributes) exactly as specified (pk, title, author, views, and comments)
+# with their corresponding values.
+
+class DataBase:
+    pk = 1
+    title = "Classes and Objects"
+    author = "Pavlo Kostyshen"
+    views = 14356
+    comments = 12

f84ebe0a85b9d48d72ffb35e6874de99ed2f5b31
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2023-04-19 19:33:23 +0300

    Update task_5.py

--- OOP/OOP_tasks/public_private_protected_setter_getter/task_5.py
+++ OOP/OOP_tasks/public_private_protected_setter_getter/task_5.py
@@ -34,3 +34,40 @@
 # 
 # Create an object rect of the Rectangle class with coordinates (0, 0), (20, 34).
 # 
+
+class Point:
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def get_coords(self):
+        return self.__x, self.__y
+
+
+class Rectangle:
+    def __init__(self, x1, y1, x2=None, y2=None):
+        if isinstance(x1, Point) and isinstance(y1, Point):
+            self.__sp = x1
+            self.__ep = y1
+        else:
+            self.__sp = Point(x1, y1)
+            self.__ep = Point(x2, y2)
+
+    def set_coords(self, sp, ep):
+        self.__sp = sp
+        self.__ep = ep
+
+    def get_coords(self):
+        return self.__sp, self.__ep
+
+    def draw(self):
+        print(f'Rectangle with coordinates: {(self.__sp.get_coords())} {(self.__ep.get_coords())}')
+
+
+rect = Rectangle(0, 0, 20, 34)
+
+
+
+
+
+

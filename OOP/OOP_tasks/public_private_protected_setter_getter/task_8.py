8c130cf5096d0c14999d9b7a27ff421db6def94e
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2023-04-23 06:32:34 +0300

    Create task_8.py

---
+++ OOP/OOP_tasks/public_private_protected_setter_getter/task_8.py
@@ -0,0 +1,16 @@
+# Declare the Car class in the program, where you implement an object property named model for writing and reading car model information from the local private variable __model.
+
+# Declare the object property using the @property decorator. Also, in the object property model, the following checks should be implemented:
+
+# the car model is a string;
+# the length of the model string should be in the range [2; 100].
+# If the check fails, the local property __model remains unchanged.
+
+# Car class objects are supposed to be created with the command:
+
+# car = Car()
+# and then work with the object property, for example:
+
+# car.model = "Toyota"
+# P.S. Declare only the class in the program. Do not output anything to the screen.
+
